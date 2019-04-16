4c8f760d69632164476d8bf6501ccf3c618c1b5b
Amy Cen <amy.cen@blackrock.com>
2019-04-16 16:02:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,4 +20,4 @@ def word_substituter(tweet)
 end
 
 puts dictionary("hello")
-word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")

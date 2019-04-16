88d2e6b0d59551ad0b34a36dd73f35646d595c7b
Amy Cen <amy.cen@blackrock.com>
2019-04-16 15:59:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,3 +20,5 @@ def word_substituter(tweet)
   test = tweet.split.map {|word| dictionary(word)}
   binding.pry
 end
+
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
